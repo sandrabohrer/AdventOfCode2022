79a8779efc18f980ae6ceeeabbd4a920c5f5dba2
Sandra Bohrer <sandra.kingsbury@gmail.com>
2022-12-02 10:46:55 -0500

    Complete part 2 of day 2 challenge

--- JavaScript/day2.js
+++ JavaScript/day2.js
@@ -116,4 +116,110 @@ function returnYourFinalScore() {
     return overallScores.youFinalScore;
 }
 
-console.log('Your final score is: ' + returnYourFinalScore());
+function returnPart2ConvertedStrategyGuide() {
+    var strategyGuide = returnStrategyGuide(),
+        arrayOfStrategies = _.invokeMap(strategyGuide, String.prototype.split, ' '),
+        opponentSelection = '',
+        outcome = '',
+        convertedStrategies = [];
+
+    _.forEach(arrayOfStrategies, function(eachStrategy, index) {
+        // The first column is what your opponent is going to play: A for Rock, B for Paper, and C for Scissors.
+        if (eachStrategy[0] === 'A') {
+            opponentSelection = 'Rock';
+        } else if (eachStrategy[0] === 'B') {
+            opponentSelection = 'Paper';
+        } else if (eachStrategy[0] === 'C') {
+            opponentSelection = 'Scissors';
+        } else {
+            opponentSelection = '';
+        }
+
+        // ... the second column says how the round needs to end:
+        // X means you need to lose, Y means you need to end the round in a draw, and Z means you need to win.
+        if (eachStrategy[1] === 'X') {
+            outcome = 'opponent';
+        } else if (eachStrategy[1] === 'Y') {
+            outcome = 'draw';
+        } else if (eachStrategy[1] === 'Z') {
+            outcome = 'you';
+        } else {
+            outcome = '';
+        }
+
+        convertedStrategies.push([opponentSelection, outcome]);
+    });
+
+    return convertedStrategies;
+}
+
+function returnYourPart2FinalScore() {
+    var strategyGuide = returnPart2ConvertedStrategyGuide(),
+        opponentScore = 0,
+        youScore = 0,
+        overallScores = {},
+        rockAmount = 1,
+        paperAmount = 2,
+        scissorsAmount = 3,
+        lossAmount = 0,
+        drawAmount = 3,
+        winAmount = 6;
+
+    _.forEach(strategyGuide, function(eachStrategy) {
+        // eachStrategy[0] = opponent
+        // eachStrategy[1] = outcome
+        // Rock defeats Scissors, Scissors defeats Paper, and Paper defeats Rock.
+
+        if (eachStrategy[0] === 'Rock' && eachStrategy[1] === 'draw') {
+            // you = rock
+            opponentScore += drawAmount + rockAmount;
+            youScore += drawAmount + rockAmount;
+        } else if (eachStrategy[0] === 'Paper' && eachStrategy[1] === 'draw') {
+            // you = paper
+            opponentScore += drawAmount + paperAmount;
+            youScore += drawAmount + paperAmount;
+        } else if (eachStrategy[0] === 'Scissors' && eachStrategy[1] === 'draw') {
+            // you = scissors
+            opponentScore += drawAmount + scissorsAmount;
+            youScore += drawAmount + scissorsAmount;
+        } else if (eachStrategy[0] === 'Rock' && eachStrategy[1] === 'you') {
+            // you = paper
+            opponentScore += lossAmount + rockAmount;
+            youScore += winAmount + paperAmount;
+        } else if (eachStrategy[0] === 'Rock' && eachStrategy[1] === 'opponent') {
+            // you = scissors
+            opponentScore += winAmount + rockAmount;
+            youScore += lossAmount + scissorsAmount;
+        } else if (eachStrategy[0] === 'Paper' && eachStrategy[1] === 'you') {
+            // you = scissors
+            opponentScore += lossAmount + paperAmount;
+            youScore += winAmount + scissorsAmount;
+        } else if (eachStrategy[0] === 'Paper' && eachStrategy[1] === 'opponent') {
+            // you = rock
+            opponentScore += winAmount + paperAmount;
+            youScore += lossAmount + rockAmount;
+        } else if (eachStrategy[0] === 'Scissors' && eachStrategy[1] === 'you') {
+            // you = rock
+            opponentScore += lossAmount + scissorsAmount;
+            youScore += winAmount + rockAmount;
+        } else if (eachStrategy[0] === 'Scissors' && eachStrategy[1] === 'opponent') {
+            // you = paper
+            opponentScore += winAmount + scissorsAmount;
+            youScore += lossAmount + paperAmount;
+        } else {
+            outcome = '';
+            youScore = 0;
+            opponentScore = 0;
+        }
+    });
+
+    overallScores = {
+        opponentFinalScore: opponentScore,
+        youFinalScore: youScore
+    };
+
+    return overallScores.youFinalScore;
+}
+
+console.log('Your part 1 final score is: ' + returnYourFinalScore());
+console.log('Your part 2 final score is: ' + returnYourPart2FinalScore());
