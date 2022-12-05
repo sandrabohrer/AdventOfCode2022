698180a9ad40f49aa56105774b26e75e21974b21
Sandra Bohrer <sandra.kingsbury@gmail.com>
2022-12-05 14:25:47 -0500

    Complete part 2 of day 4 challenge

--- JavaScript/day4.js
+++ JavaScript/day4.js
@@ -27,7 +27,7 @@ function returnSplitSectionAssignments() {
     return newSectionAssignments;
 }
 
-function returnSectionAssignmentCheck() {
+function returnFullSectionAssignmentOverlapCheck() {
     var sectionAssignments = returnSplitSectionAssignments()
         elfAssignment1 = [],
         elfAssignment2 = [],
@@ -69,4 +69,47 @@ function returnSectionAssignmentCheck() {
     return sectionAssignmentOverlapCount;
 }
 
-console.log('The number of assignment pairs where one range fully contains the other is: ' + returnSectionAssignmentCheck());
+function returnPartialSectionAssignmentOverlapCheck() {
+    var sectionAssignments = returnSplitSectionAssignments()
+        elfAssignment1 = [],
+        elfAssignment2 = [],
+        sectionAssignmentOverlapCount = 0,
+        elf1Start = 0,
+        elf1End = 0,
+        elf2Start = 0,
+        elf2End = 0,
+        isFirstRangeInSecondRange = false,
+        isSecondRangeInFirstRange = false;
+
+    _.forEach(sectionAssignments, function(elfPair) {
+        elf1Start = 0;
+        elf1End = 0;
+        elf2Start = 0;
+        elf2End = 0;
+        isFirstRangeInSecondRange = false;
+        isSecondRangeInFirstRange = false;
+
+        elfAssignment1 = elfPair[0].split('-');
+        elfAssignment2 = elfPair[1].split('-');
+
+        elfAssignment1 = elfAssignment1.map(Number);
+        elfAssignment2 = elfAssignment2.map(Number);
+
+        elf1Start = elfAssignment1[0];
+        elf1End = elfAssignment1[1] + 1;
+        elf2Start = elfAssignment2[0];
+        elf2End = elfAssignment2[1] + 1;
+
+        isFirstRangeInSecondRange = _.inRange(elfAssignment1[0], elf2Start, elf2End) || _.inRange(elfAssignment1[1], elf2Start, elf2End);
+        isSecondRangeInFirstRange = _.inRange(elfAssignment2[0], elf1Start, elf1End) || _.inRange(elfAssignment2[1], elf1Start, elf1End);
+
+        if (isFirstRangeInSecondRange || isSecondRangeInFirstRange) {
+            sectionAssignmentOverlapCount += 1;
+        }
+    });
+
+    return sectionAssignmentOverlapCount;
+}
+
+console.log('The number of assignment pairs where one range fully contains the other is: ' + returnFullSectionAssignmentOverlapCheck());
+console.log('The number of assignment pairs where the ranges overlap is: ' + returnPartialSectionAssignmentOverlapCheck());
